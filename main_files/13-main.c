1c9cfecac9d8d8f152b208de00e463a84a0802f5
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-02-28 01:45:58 +0200

    test

--- main_files/13-main.c
+++ main_files/13-main.c
@@ -10,6 +10,7 @@
 int main(void)
 {
     binary_tree_t *root;
+    size_t nodes;
 
     root = binary_tree_node(NULL, 98);
     root->left = binary_tree_node(root, 12);
@@ -17,6 +18,12 @@ int main(void)
     binary_tree_insert_right(root->left, 54);
     binary_tree_insert_right(root, 128);
     binary_tree_print(root);
-    binary_tree_delete(root);
+
+    nodes = binary_tree_nodes(root);
+    printf("Nodes in %d: %lu\n", root->n, nodes);
+    nodes = binary_tree_nodes(root->right);
+    printf("Nodes in %d: %lu\n", root->right->n, nodes);
+    nodes = binary_tree_nodes(root->left->right);
+    printf("Nodes in %d: %lu\n", root->left->right->n, nodes);
     return (0);
 }
