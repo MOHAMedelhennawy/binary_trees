c1d80027017785a1871eeb04280616c69392e650
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-02-29 04:40:27 +0200

    Fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,7 +5,6 @@
  * @parent: is a pointer to the node to insert the left-child in.
  * @value: is the value to store in the new node.
  */
-
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
