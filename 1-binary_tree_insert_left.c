9e5c9c51c6df5384730558cffbe077e3df323a3e
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2025-02-15 16:24:34 +0200

    Fix Taks 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,30 +1,27 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - nserts a node as the left-child of another node
+ * binary_tree_insert_left - Inserts a node as the left-child of another node
  * @parent: node parent
  * @value: Value of the node
  * Return: the new node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *BNode;
+
 	if (!parent)
 		return (NULL);
 
-	binary_tree_t *BNode = binary_tree_node(parent, value);
-
+	BNode = binary_tree_node(parent, value);
 	if (!BNode)
 		return (NULL);
 
-	if (!parent->left)
-		parent->left = BNode;
-	else
+	if (parent->left)
 	{
-		binary_tree_t *tmp = parent->left;
-
-		parent->left = BNode;
-		BNode->left = tmp;
-		tmp->parent = BNode;
+		BNode->left = parent->left;
+		parent->left->parent = BNode;
 	}
 
+	parent->left = BNode;
 	return (BNode);
 }
